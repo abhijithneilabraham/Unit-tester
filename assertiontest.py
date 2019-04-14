6d3e9e60893c4dbe67cf60eeec8f23ddbaedc198
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2019-04-14 23:48:09 +0530

    Update assertiontest.py

--- assertiontest.py
+++ assertiontest.py
@@ -6,4 +6,11 @@ Created on Sun Apr 14 19:58:10 2019
 @author: abhijithneilabraham
 """
 
-assert sum([1, 1, 3]) == 6, "Should be 6"
+
+def test_sum():
+    assert sum([1, 2, 3]) == 6, "Should be 6"
+
+if __name__ == "__main__":
+    test_sum()
+    print("Everything passed")
+
